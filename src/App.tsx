5feea081e83237b32634b09b1e0c4c86b21b8b70
drxwat <drxwat@gmail.com>
2023-10-13 19:09:46 +0700

    mui icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   // const email = import.meta.env.VITE_USER_EMAIL;
   // const password = import.meta.env.VITE_SUPABASE_PUBLIC_KEY;
   // const client = await buildClient(email, password);
+
   return (
     <Suspense fallback={<div>"Loading..."</div>}>
       <button>Request</button>
