b63be3b646be02cbd66e414614afbf0ee4277cf4
drxwat <drxwat@gmail.com>
2023-10-13 18:38:34 +0700

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Suspense } from "react";
 
 function App() {
+  // const email = import.meta.env.VITE_USER_EMAIL;
+  // const password = import.meta.env.VITE_SUPABASE_PUBLIC_KEY;
+  // const client = await buildClient(email, password);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => {}}>REQUEST</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Suspense fallback={<div>"Loading..."</div>}>
+      <button>Request</button>
+    </Suspense>
   );
 }
 
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,16 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
 
-export const buildClient = async () => {
+export const buildClient = async (email: string, password: string) => {
   const supabase = createClient<Database>(
     import.meta.env.VITE_SUPABASE_PROJECT_URL,
     import.meta.env.VITE_SUPABASE_PUBLIC_KEY
   );
 
-  await supabase.auth.signInWithPassword({
-    email: import.meta.env.VITE_USER_EMAIL,
-    password: import.meta.env.VITE_SUPABASE_PUBLIC_KEY,
-  });
+  await supabase.auth.signInWithPassword({ email, password });
 
   return supabase;
 };
