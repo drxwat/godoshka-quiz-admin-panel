221db6d2e991b32848c5caedcba512d43cce8a5a
Renat Mardanov <rent1995@ya.ru>
2023-10-14 15:16:53 +0700

    feat: add login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { Box } from "@mui/material";
-import { Suspense } from "react";
+import { LoginForm } from "./Components/Forms/login.tsx";
 
 function App() {
-  // const email = import.meta.env.VITE_USER_EMAIL;
-  // const password = import.meta.env.VITE_SUPABASE_PUBLIC_KEY;
-  // const client = await buildClient(email, password);
-
-  return (
-    <Suspense fallback={<div>"Loading..."</div>}>
-      <Box>
-        <button>Request</button>
-      </Box>
-    </Suspense>
-  );
+    return <LoginForm />;
 }
 
 export default App;
