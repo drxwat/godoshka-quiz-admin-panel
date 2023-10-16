03cdb1ead4f77309f79ec3a931ecf885d51f3244
drxwat <drxwat@gmail.com>
2023-10-16 14:50:20 +0700

    env vars fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./core/routes";
 
+console.log(import.meta.env);
+
 const router = createBrowserRouter(routes, {
   basename: import.meta.env.BASE_URL,
 });
