88a0541493a1e461bfcbf14712623d3e1987eca9
Renat Mardanov <rent1995@ya.ru>
2023-10-19 18:25:04 +0700

    fix: add navigation: question -> modules

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,20 +1,39 @@
-import { Box, Button, Typography } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import { useUserData } from "../hooks/useUserData";
 import { useAuth } from "../core/auth/useAuth";
+import { GodotLogo } from "../assets/godot.logo";
+import { useNavigate } from "react-router";
 
 export const NavigationPanel = () => {
   const { email } = useUserData();
   const logout = useAuth();
+  const navigate = useNavigate();
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-around",
-      }}
-    >
-      <Typography>Привет, {email}</Typography>
-      <Button onClick={() => logout.signOut()}>Выйти</Button>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => navigate("/")}
+          >
+            <GodotLogo />
+          </IconButton>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography>{email}</Typography>
+            <Button color="inherit" onClick={() => logout.signOut()}>
+              Logout
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
     </Box>
   );
 };
